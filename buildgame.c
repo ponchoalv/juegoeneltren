af3098623957c4834920a4b9d66f8ca1c37d6074
Alfonso Alvarez <alfonso.alvarez@loveholidays.com>
2026-07-14 15:47:39 +0100

    chaged the buildgame.c, is not running qt creator anymore.
    This is due I'm trying different editors and ways for launching a debuger

--- buildgame.c
+++ buildgame.c
@@ -105,13 +105,13 @@ int main(int argc, char **argv)
             if (!nob_cmd_run(&cmd))
                 return 1;
 
-            const char *currentDir = nob_get_current_dir_temp();
-            const char *bpath = nob_temp_sprintf("%s/%s", currentDir, BUILD_FOLDER "juego_en_el_tren");
-            const char *spath = nob_temp_sprintf("%s/%s", currentDir, "main.c");
+            // const char *currentDir = nob_get_current_dir_temp();
+            // const char *bpath = nob_temp_sprintf("%s/%s", currentDir, BUILD_FOLDER "juego_en_el_tren");
+            // const char *spath = nob_temp_sprintf("%s/%s", currentDir, "main.c");
 
-            nob_cmd_append(&cmd, "open", "-a", "Qt Creator", "--args", spath, "-debug", bpath);
-            if (!nob_cmd_run(&cmd))
-                return 1;
+            // nob_cmd_append(&cmd, "open", "-a", "Qt Creator", "--args", spath, "-debug", bpath);
+            // if (!nob_cmd_run(&cmd))
+            //     return 1;
         }
     }
 
