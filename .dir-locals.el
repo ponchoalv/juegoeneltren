1c0ef987e076a5a99cad9e5ee7d21514d756ce4f
Alfonso Alvarez <alfonso.alvarez@loveholidays.com>
2026-07-27 17:22:48 +0100

    added .dir-locals.el for debugging inside emacs with dape

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,8 @@
 ;; ((nil . ((compile-command . "clang -std=c99 -Wall -Wextra -Wpedantic -framework CoreVideo -framework IOKit -framework Cocoa -framework GLUT -framework OpenGL libraylib.a main.c -o build/juego_en_el_tren && ./build/juego_en_el_tren"))))
 
+((nil . ((dape-command . (lldb-dap
+                           :program "build/juego_en_el_tren"
+                           compile "./buildgame -debug"))
+         (compile-command . "./buildgame -run"))))
 ;; for generating tags I recommend using ctags (universal c-tags) with this option
 ;; ctags -e -R --c-kinds=+p --c++-kinds=+p .
